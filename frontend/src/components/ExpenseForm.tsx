c219ed27f00e3c0466801ab0e31e5e09c4b69bd4
Daisy Viscaya <daisyreeviscaya@gmail.com>
2026-07-09 01:54:43 +0800

    Add noValidate to Add expense form so it can use customized validations set

--- frontend/src/components/ExpenseForm.tsx
+++ frontend/src/components/ExpenseForm.tsx
@@ -57,7 +57,7 @@ export function ExpenseForm({
   }));
 
   return (
-    <form onSubmit={handleSubmit} style={formStyle}>
+    <form onSubmit={handleSubmit} noValidate style={formStyle}>
       <TextField
         label="Amount"
         type="number"
